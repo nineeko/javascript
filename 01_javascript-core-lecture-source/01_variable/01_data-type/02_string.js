06f67792e35b33406ae99a8d7d414bc53ef41e0a
nineeko <gkxm1013@naver.com>
2025-04-18 12:49:10 +0900

    25.04.18 pm 12:48

---
+++ 01_javascript-core-lecture-source/01_variable/01_data-type/02_string.js
@@ -0,0 +1,15 @@
+var string;
+string = 'JavaScript';
+string = "JavaScript";
+string = `JavaScript`; // 백틱
+
+// 템플릿 리터럴 : ES6부터 도입된 멀티라인 문자열, 표현식 삽입
+var str =`안녕하세요
+    반갑습니다
+    제 이름은 홍길동입니다.`;
+console.log(str);
+
+var lastName = '홍';
+var firstName = '길동';
+console.log("제 이름은 "+ lastName + firstName+ "입니다.");
+console.log(`제 이름은 ${lastName}${firstName}입니다.`);
