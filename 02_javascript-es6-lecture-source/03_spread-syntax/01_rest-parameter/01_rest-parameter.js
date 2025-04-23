fdd4bf8b47e1c5015259a2e3c5d43056ff4ca421
nineeko <gkxm1013@naver.com>
2025-04-23 09:44:37 +0900

    25.04.23 am 9:44

---
+++ 02_javascript-es6-lecture-source/03_spread-syntax/01_rest-parameter/01_rest-parameter.js
@@ -0,0 +1,18 @@
+// 나머지 매개변수
+function merge(msg1, msg2) {
+    return msg1 + msg2;
+}
+
+console.log(merge("안녕하세요. "));
+console.log(merge('안녕하세요.', ' 반갑습니다.'));
+console.log(merge('안녕하세요.', ' 반갑습니다.',' 제 이름은 홍길동입니다.'));
+
+// 위의 상황을 나머지 매개변수를 활용해서 변경해본다.
+function mergeAll(...args) {   // 몇개의 매개변수가 넘어올지 정해두지 않음
+    let msg = '';
+    for(let arg of args) { msg += arg; }
+    return msg;
+}
+console.log(mergeAll("안녕하세요."));
+console.log(mergeAll('안녕하세요.', ' 반갑습니다.'));
+console.log(mergeAll('안녕하세요.', ' 반갑습니다.',' 제 이름은 홍길동입니다.'));
---
+++ 02_javascript-es6-lecture-source/04_distructuring-assignment/02_object-distructuring-assignment/01_object-distructuring-assignment.js
@@ -0,0 +1,13 @@
+// 객체 구조 분해 할당
+const pants = {
+    productName : '배기 팬츠',
+    color : "검정색",
+    price : 30000
+};
+const { productName, price, color} = pants; // 순서 바껴도 상관X
+console.log(productName);
+console.log(color);
+console.log(price);
+
+// 각 변수의 서술 순서는 무관하며 { 객체 프로퍼티 : 목표 변수 } 형식으로 작성하여
+// 프로퍼티 키 값과 다른 변수명을 설정할 수도 있다.
